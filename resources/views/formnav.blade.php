4d377a66fb7228cda51a3ce83bc803a8b2be8da9
thiate04 <105304589+thiate04@users.noreply.github.com>
2022-12-17 23:26:32 +0000

    last comMIT

--- resources/views/formnav.blade.php
+++ resources/views/formnav.blade.php
@@ -18,7 +18,7 @@
 	<meta name="twitter:card" content="summary_large_image"> <!-- to have large image post format in Twitter -->
 
     <!-- Webpage Title -->
-    <title>Assistancia_rec | @yield('headtitle')</title>
+    <title>Assistancia | @yield('headtitle')</title>
     <!-- Styles -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&family=Poppins:wght@600&display=swap" rel="stylesheet">
