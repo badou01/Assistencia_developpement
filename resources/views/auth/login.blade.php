6373d980d835dab0f669385c16918a6f6cfc0d79
thiate04 <105304589+thiate04@users.noreply.github.com>
2022-12-18 00:33:34 +0000

    dernier 00:33

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>CodePen - Glassmorphism login Form Tutorial in html css</title>
+  <title>Login</title>
 
 
 </head>
@@ -13,7 +13,7 @@
 <html lang="en">
 <head>
   <!-- Design by foolishdeveloper.com -->
-    <title>Glassmorphism login Form Tutorial in html css</title>
+    <title>Login</title>
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
@@ -151,63 +151,7 @@ button{
         <div class="shape"></div>
         <div class="shape"></div>
     </div>
-    {{-- <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <div class="row mb-3">
-            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Adresse e-mail') }}</label>
-
-            <div class="col-md-6">
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"  required autocomplete="email" autofocus>
-
-                @error('email')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-        </div>
-
-        <div class="row mb-3">
-            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Mot de passe') }}</label>
-
-            <div class="col-md-6">
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-        </div>
-
-        <div class="row mb-3">
-            <div class="col-md-6 offset-md-4">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                    <label class="form-check-label" for="remember">
-                        {{ __('Remember Me') }}
-                    </label>
-                </div>
-            </div>
-        </div>
-
-        <div class="row mb-0">
-            <div class="col-12 offset-md-3">
-                <button type="submit" class="btn btn-primary">
-                    {{ __('Login') }}
-                </button>
-
-                @if (Route::has('password.request'))
-                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                        {{ __('Mot de passe Oublié?') }}
-                    </a>
-                @endif
-            </div>
-        </div>
-    </form> --}}
+  
 
 
     <form method="POST" action="{{ route('login') }}">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>CodePen - Glassmorphism login Form Tutorial in html css</title>
+  <title>Register</title>
 
 
 </head>
@@ -13,7 +13,7 @@
 <html lang="en">
 <head>
   <!-- Design by foolishdeveloper.com -->
-    <title>Glassmorphism login Form Tutorial in html css</title>
+    <title>Register</title>
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
@@ -152,107 +152,7 @@ button{
         <div class="shape"></div>
         <div class="shape"></div>
     </div>
-    {{-- <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
-        @csrf
 
-        <div class="row mb-3">
-            <label for="nom" class="col-md-4 col-form-label text-md-end">{{ __('Nom') }}</label>
-
-            <div class="col-md-6">
-                <input id="nom" type="text" class="form-control @error('nom') is-invalid @enderror" name="nom" value="{{ old('nom') }}" required autocomplete="name" autofocus>
-
-                @error('nom')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-        </div>
-        <div class="row mb-3">
-            <label for="prenom" class="col-md-4 col-form-label text-md-end">{{ __('Prenom') }}</label>
-
-            <div class="col-md-6">
-                <input id="prenom" type="text" class="form-control @error('prenom') is-invalid @enderror" name="prenom" value="{{ old('prenom') }}" required autocomplete="given-name" autofocus>
-
-                @error('prenom')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-        </div>
-        <div class="row mb-3">
-            <label for="nom_utilisateur" class="col-md-4 col-form-label text-md-end">{{ __('Nom d\'utilisateur') }}</label>
-
-            <div class="col-md-6">
-                <input id="nom_utilisateur" type="text" class="form-control @error('nom_utilisateur') is-invalid @enderror" name="nom_utilisateur" value="{{ old('nom_utilisateur') }}" required autocomplete="username" autofocus>
-
-                @error('nom_utilisateur')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-        </div>
-
-        <div class="row mb-3">
-            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Addresse email') }}</label>
-
-            <div class="col-md-6">
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                @error('email')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-        </div>
-        <div class="row mb-3">
-            <label for="photo_identite" class="col-md-4 col-form-label text-md-end">{{ __('Photo d\'identité') }}</label>
-
-            <div class="col-md-6">
-                <input id="photo_identite" type="file" class="form-control @error('photo_identite') is-invalid @enderror" name="photo_identite"  autocomplete="photo">
-
-                @error('photo_identite')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-        </div>
-
-
-        <div class="row mb-3">
-            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Mot de passe') }}</label>
-
-            <div class="col-md-6">
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-        </div>
-
-        <div class="row mb-3">
-            <label for="password_confirmation" class="col-md-4 col-form-label text-md-end">{{ __('Confirmer mot de passe') }}</label>
-
-            <div class="col-md-6">
-                <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-            </div>
-        </div>
-
-        <div class="row mb-0">
-            <div class="col-md-6 offset-md-4">
-                <button type="submit" class="btn btn-primary">
-                    {{ __('S\'inscrire') }}
-                </button>
-            </div>
-        </div>
-    </form> --}}
 
     <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
         @csrf
